33cb3bdf5fb974343800e34ea313b5da5b27201f
Jovi <redzjovi@gmail.com>
2017-05-23 17:50:07 +0700

    Adding classes to rows in gridview.

--- advanced/backend/views/branches/index.php
+++ advanced/backend/views/branches/index.php
@@ -23,6 +23,13 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
+        'rowOptions' => function ($model) {
+            if ($model->branch_status == 'active') {
+                return ['class' => 'success'];
+            } else if ($model->branch_status == 'inactive') {
+                return ['class' => 'danger'];
+            }
+        },
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
             [
