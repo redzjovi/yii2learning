0817f6de918242dbf0f578a6f773cc6b784bd415
Jovi <redzjovi@gmail.com>
2017-05-25 17:29:55 +0700

    Dynamic form.

--- yii2advanced.sql
+++ yii2advanced.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Jan 29, 2015 at 02:52 PM
--- Server version: 5.5.41-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.5
+-- Host: 127.0.0.1
+-- Generation Time: May 24, 2017 at 12:09 PM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,10 +14,10 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `advanced_yii2`
+-- Database: `yii2advanced`
 --
 
 -- --------------------------------------------------------
@@ -26,11 +26,11 @@ SET time_zone = "+00:00";
 -- Table structure for table `auth_assignment`
 --
 
-CREATE TABLE IF NOT EXISTS `auth_assignment` (
+DROP TABLE IF EXISTS `auth_assignment`;
+CREATE TABLE `auth_assignment` (
   `item_name` varchar(64) NOT NULL,
-  `user_id` varchar(64) NOT NULL,
-  `created_at` int(11) DEFAULT NULL,
-  PRIMARY KEY (`item_name`,`user_id`)
+  `user_id` int(11) NOT NULL,
+  `created_at` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -38,8 +38,10 @@ CREATE TABLE IF NOT EXISTS `auth_assignment` (
 --
 
 INSERT INTO `auth_assignment` (`item_name`, `user_id`, `created_at`) VALUES
-('admin', '1', NULL),
-('admin', '2', NULL);
+('admin', 1, NULL),
+('admin', 2, NULL),
+('admin', 5, NULL),
+('create-branch', 5, NULL);
 
 -- --------------------------------------------------------
 
@@ -47,17 +49,15 @@ INSERT INTO `auth_assignment` (`item_name`, `user_id`, `created_at`) VALUES
 -- Table structure for table `auth_item`
 --
 
-CREATE TABLE IF NOT EXISTS `auth_item` (
+DROP TABLE IF EXISTS `auth_item`;
+CREATE TABLE `auth_item` (
   `name` varchar(64) NOT NULL,
   `type` int(11) NOT NULL,
   `description` text,
   `rule_name` varchar(64) DEFAULT NULL,
   `data` text,
   `created_at` int(11) DEFAULT NULL,
-  `updated_at` int(11) DEFAULT NULL,
-  PRIMARY KEY (`name`),
-  KEY `rule_name` (`rule_name`),
-  KEY `type` (`type`)
+  `updated_at` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -76,11 +76,10 @@ INSERT INTO `auth_item` (`name`, `type`, `description`, `rule_name`, `data`, `cr
 -- Table structure for table `auth_item_child`
 --
 
-CREATE TABLE IF NOT EXISTS `auth_item_child` (
+DROP TABLE IF EXISTS `auth_item_child`;
+CREATE TABLE `auth_item_child` (
   `parent` varchar(64) NOT NULL,
-  `child` varchar(64) NOT NULL,
-  PRIMARY KEY (`parent`,`child`),
-  KEY `child` (`child`)
+  `child` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -97,12 +96,12 @@ INSERT INTO `auth_item_child` (`parent`, `child`) VALUES
 -- Table structure for table `auth_rule`
 --
 
-CREATE TABLE IF NOT EXISTS `auth_rule` (
+DROP TABLE IF EXISTS `auth_rule`;
+CREATE TABLE `auth_rule` (
   `name` varchar(64) NOT NULL,
   `data` text,
   `created_at` int(11) DEFAULT NULL,
-  `updated_at` int(11) DEFAULT NULL,
-  PRIMARY KEY (`name`)
+  `updated_at` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -118,25 +117,25 @@ INSERT INTO `auth_rule` (`name`, `data`, `created_at`, `updated_at`) VALUES
 -- Table structure for table `branches`
 --
 
-CREATE TABLE IF NOT EXISTS `branches` (
-  `branch_id` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `branches`;
+CREATE TABLE `branches` (
+  `branch_id` int(11) NOT NULL,
   `companies_company_id` int(11) NOT NULL,
   `branch_name` varchar(100) NOT NULL,
   `branch_address` varchar(255) NOT NULL,
   `branch_created_date` datetime NOT NULL,
-  `branch_status` enum('active','inactive') NOT NULL,
-  PRIMARY KEY (`branch_id`),
-  KEY `fk_branches_companies_idx` (`companies_company_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+  `branch_status` enum('active','inactive') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `branches`
 --
 
 INSERT INTO `branches` (`branch_id`, `companies_company_id`, `branch_name`, `branch_address`, `branch_created_date`, `branch_status`) VALUES
-(2, 2, 'main branch', 'some branch address', '2014-12-05 06:12:27', 'active'),
+(2, 2, 'main branch', 'some branch address', '2014-12-05 06:12:27', 'inactive'),
 (3, 2, 'another branch', 'another branch address', '2014-12-05 07:12:16', 'active'),
-(4, 3, 'Sri Lanka', 'sdfdfsdf', '2014-12-15 01:12:24', 'active');
+(4, 3, 'Sri Lanka', 'sdfdfsdf', '2014-12-15 01:12:24', 'active'),
+(5, 2, 'Branch Name 2', 'Branch Address 2', '2017-05-21 15:34:14', 'active');
 
 -- --------------------------------------------------------
 
@@ -144,17 +143,17 @@ INSERT INTO `branches` (`branch_id`, `companies_company_id`, `branch_name`, `bra
 -- Table structure for table `companies`
 --
 
-CREATE TABLE IF NOT EXISTS `companies` (
-  `company_id` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `companies`;
+CREATE TABLE `companies` (
+  `company_id` int(11) NOT NULL,
   `company_name` varchar(100) NOT NULL,
   `company_email` varchar(100) NOT NULL,
   `company_address` varchar(255) NOT NULL,
   `logo` varchar(200) NOT NULL,
   `company_start_date` date NOT NULL,
   `company_created_date` datetime NOT NULL,
-  `company_status` enum('active','inactive') NOT NULL,
-  PRIMARY KEY (`company_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
+  `company_status` enum('active','inactive') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `companies`
@@ -163,7 +162,25 @@ CREATE TABLE IF NOT EXISTS `companies` (
 INSERT INTO `companies` (`company_id`, `company_name`, `company_email`, `company_address`, `logo`, `company_start_date`, `company_created_date`, `company_status`) VALUES
 (2, 'ABC', 'abc@gmail.com', 'some address', '', '0000-00-00', '2014-12-05 06:12:19', 'active'),
 (3, 'DoingITeasy', 'do@sdf.com', 'sodfksdfsdf', '', '0000-00-00', '2014-12-15 01:12:47', 'active'),
-(8, 'DoingITeasyChannel', 'doi@doing.gi', 'some address', 'uploads/DoingITeasyChannel.png', '0000-00-00', '2015-01-10 08:01:30', 'active');
+(8, 'DoingITeasyChannel', 'doi@doing.gi', 'some address', 'uploads/DoingITeasyChannel.png', '0000-00-00', '2015-01-10 08:01:30', 'active'),
+(9, 'Company Name 1', 'email@email.com', 'Company Address 1', '', '0000-00-00', '2017-05-21 15:30:47', 'active'),
+(10, 'Company Name 1', 'email@email.com', 'Company Address 1', 'Logo', '2017-05-31', '2017-05-31 00:00:00', 'active'),
+(11, 'BodyFitStation', 'redzjovi@gmail.com', 'Jelambar', 'uploads/BodyFitStation.jpg', '0000-00-00', '2017-05-23 09:11:04', 'active');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+CREATE TABLE `customers` (
+  `customer_id` int(11) NOT NULL,
+  `customer_name` varchar(100) NOT NULL,
+  `zip_code` varchar(20) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `province` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -171,24 +188,23 @@ INSERT INTO `companies` (`company_id`, `company_name`, `company_email`, `company
 -- Table structure for table `departments`
 --
 
-CREATE TABLE IF NOT EXISTS `departments` (
-  `department_id` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `departments`;
+CREATE TABLE `departments` (
+  `department_id` int(11) NOT NULL,
   `branches_branch_id` int(11) NOT NULL,
   `department_name` varchar(100) NOT NULL,
   `companies_company_id` int(11) NOT NULL,
   `department_created_date` datetime NOT NULL,
-  `department_status` enum('active','inactive') NOT NULL,
-  PRIMARY KEY (`department_id`),
-  KEY `fk_departments_branches1_idx` (`branches_branch_id`),
-  KEY `fk_departments_companies1_idx` (`companies_company_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+  `department_status` enum('active','inactive') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `departments`
 --
 
 INSERT INTO `departments` (`department_id`, `branches_branch_id`, `department_name`, `companies_company_id`, `department_created_date`, `department_status`) VALUES
-(2, 2, 'IT Department', 2, '2014-12-05 06:12:09', 'active');
+(2, 2, 'IT Department', 2, '2014-12-05 06:12:09', 'active'),
+(3, 2, 'Department Name 2', 2, '2017-05-21 15:38:57', 'active');
 
 -- --------------------------------------------------------
 
@@ -196,15 +212,15 @@ INSERT INTO `departments` (`department_id`, `branches_branch_id`, `department_na
 -- Table structure for table `emails`
 --
 
-CREATE TABLE IF NOT EXISTS `emails` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `emails`;
+CREATE TABLE `emails` (
+  `id` int(11) NOT NULL,
   `receiver_name` varchar(50) NOT NULL,
   `receiver_email` varchar(200) NOT NULL,
   `subject` varchar(255) NOT NULL,
   `content` text NOT NULL,
-  `attachment` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+  `attachment` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `emails`
@@ -216,13 +232,35 @@ INSERT INTO `emails` (`id`, `receiver_name`, `receiver_email`, `subject`, `conte
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `locations`
+--
+
+DROP TABLE IF EXISTS `locations`;
+CREATE TABLE `locations` (
+  `location_id` int(11) NOT NULL,
+  `zip_code` varchar(20) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `province` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `locations`
+--
+
+INSERT INTO `locations` (`location_id`, `zip_code`, `city`, `province`) VALUES
+(1, '111', 'Colombo', 'Western'),
+(2, '222', 'Galle', 'Southern');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `migration`
 --
 
-CREATE TABLE IF NOT EXISTS `migration` (
+DROP TABLE IF EXISTS `migration`;
+CREATE TABLE `migration` (
   `version` varchar(180) NOT NULL,
-  `apply_time` int(11) DEFAULT NULL,
-  PRIMARY KEY (`version`)
+  `apply_time` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -236,11 +274,39 @@ INSERT INTO `migration` (`version`, `apply_time`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `po`
+--
+
+DROP TABLE IF EXISTS `po`;
+CREATE TABLE `po` (
+  `id` int(11) NOT NULL,
+  `po_no` varchar(10) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `po_item`
+--
+
+DROP TABLE IF EXISTS `po_item`;
+CREATE TABLE `po_item` (
+  `id` int(11) NOT NULL,
+  `po_item_no` int(10) NOT NULL,
+  `quantity` double NOT NULL,
+  `po_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
   `first_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `last_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -251,9 +317,8 @@ CREATE TABLE IF NOT EXISTS `user` (
   `role` smallint(6) NOT NULL DEFAULT '10',
   `status` smallint(6) NOT NULL DEFAULT '10',
   `created_at` int(11) NOT NULL,
-  `updated_at` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+  `updated_at` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `user`
@@ -261,8 +326,153 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 INSERT INTO `user` (`id`, `first_name`, `last_name`, `username`, `auth_key`, `password_hash`, `password_reset_token`, `email`, `role`, `status`, `created_at`, `updated_at`) VALUES
 (1, '', '', 'doingITeasy', 'mXoICMvTR_n21FMJaCyA9ft_b4RsF0Vs', '$2y$13$bucEc1sMCJyePLhjZYnDxOk0u0p7pyotQJQgd95o20G05RJ4UHoDy', NULL, 'uiheenatigala@gmail.com', 10, 10, 1416813431, 1416813431),
-(2, 'Uthpala', 'Heenatigala', 'uthpala', 'fqWNg-v9cWNLv6duFyJCBHmuAhtiCuNv', '$2y$13$Ci4zoXaf.skJ3akK/fBM/.bqUgkIqTJ7si6IJq1tybnYCbNd/FH5S', NULL, 'test@gmail.com', 10, 10, 1417756860, 1417756860);
+(2, 'Uthpala', 'Heenatigala', 'uthpala', 'fqWNg-v9cWNLv6duFyJCBHmuAhtiCuNv', '$2y$13$Ci4zoXaf.skJ3akK/fBM/.bqUgkIqTJ7si6IJq1tybnYCbNd/FH5S', NULL, 'test@gmail.com', 10, 10, 1417756860, 1417756860),
+(5, 'J', 'O', 'redzjovi', '_mLH9xoPp7DLc2e32Yn8k6KoFBj_Aezk', '$2y$13$eIttcCDI.xia7OvagxvWL.B3DGGaJTctAvyxguP8blZoGp7ZfSFAi', NULL, 'redzjovi@gmail.com', 10, 10, 1495607222, 1495607222);
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `auth_assignment`
+--
+ALTER TABLE `auth_assignment`
+  ADD PRIMARY KEY (`item_name`,`user_id`);
+
+--
+-- Indexes for table `auth_item`
+--
+ALTER TABLE `auth_item`
+  ADD PRIMARY KEY (`name`),
+  ADD KEY `rule_name` (`rule_name`),
+  ADD KEY `type` (`type`);
+
+--
+-- Indexes for table `auth_item_child`
+--
+ALTER TABLE `auth_item_child`
+  ADD PRIMARY KEY (`parent`,`child`),
+  ADD KEY `child` (`child`);
+
+--
+-- Indexes for table `auth_rule`
+--
+ALTER TABLE `auth_rule`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Indexes for table `branches`
+--
+ALTER TABLE `branches`
+  ADD PRIMARY KEY (`branch_id`),
+  ADD KEY `fk_branches_companies_idx` (`companies_company_id`);
+
+--
+-- Indexes for table `companies`
+--
+ALTER TABLE `companies`
+  ADD PRIMARY KEY (`company_id`);
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`customer_id`);
+
+--
+-- Indexes for table `departments`
+--
+ALTER TABLE `departments`
+  ADD PRIMARY KEY (`department_id`),
+  ADD KEY `fk_departments_branches1_idx` (`branches_branch_id`),
+  ADD KEY `fk_departments_companies1_idx` (`companies_company_id`);
+
+--
+-- Indexes for table `emails`
+--
+ALTER TABLE `emails`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `locations`
+--
+ALTER TABLE `locations`
+  ADD PRIMARY KEY (`location_id`);
+
+--
+-- Indexes for table `migration`
+--
+ALTER TABLE `migration`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- Indexes for table `po`
+--
+ALTER TABLE `po`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `po_item`
+--
+ALTER TABLE `po_item`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `po_id` (`po_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `branches`
+--
+ALTER TABLE `branches`
+  MODIFY `branch_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `companies`
+--
+ALTER TABLE `companies`
+  MODIFY `company_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `departments`
+--
+ALTER TABLE `departments`
+  MODIFY `department_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `emails`
+--
+ALTER TABLE `emails`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `locations`
+--
+ALTER TABLE `locations`
+  MODIFY `location_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `po`
+--
+ALTER TABLE `po`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `po_item`
+--
+ALTER TABLE `po_item`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- Constraints for dumped tables
 --
@@ -298,6 +508,12 @@ ALTER TABLE `branches`
 ALTER TABLE `departments`
   ADD CONSTRAINT `fk_departments_branches1` FOREIGN KEY (`branches_branch_id`) REFERENCES `branches` (`branch_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_departments_companies1` FOREIGN KEY (`companies_company_id`) REFERENCES `companies` (`company_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `po_item`
+--
+ALTER TABLE `po_item`
+  ADD CONSTRAINT `po_item_ibfk_1` FOREIGN KEY (`po_id`) REFERENCES `po` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
